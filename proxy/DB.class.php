f6d7efa97ca272c31503d164c2a93d531474bb84
Ranieri <ranierivalenca@gmail.com>
2019-10-02 17:36:19 -0300

    DB class now reacts to absence of cache database

--- proxy/DB.class.php
+++ proxy/DB.class.php
@@ -4,7 +4,15 @@ class DB {
     private $dbConn;
 
     private function __construct($db) {
-        $this->dbConn = new PDO("sqlite:{$db}.sqlite");
+        $dbName = "{$db}.db";
+        if (!is_file($dbName)) {
+            if (!is_file("{$db}.sql")) {
+                throw new Exception("Can't find or create database {$db}", 1);
+                exit();
+            }
+            (new SQLite3($dbName))->exec(file_get_contents("{$db}.sql"));
+        }
+        $this->dbConn = new PDO("sqlite:{$dbName}");
     }
 
     public static function getInstance($db) {
